c3c9ce5489c94f02fb97396016b656efc16ed4df
Kaden Cowan <k947g323@gmail.com>
2022-03-09 10:44:03 -0800

    GIT_AUTHOR_EMAIL="k947g323@gmail.com" && GIT_AUTHOR_NAME="Kaden Cowan" && git commit

--- src/main/model/Food.java
+++ src/main/model/Food.java
@@ -4,10 +4,44 @@ import main.Exceptions.InvalidInputException;
 import main.Exceptions.InvalidMenuSelectionException;
 
 public abstract class Food {
-    public static int counter;
-    public static int calories;
-    public static String name;
-    public static int total;
+    static int counter;
+    static int calories;
+    static String name;
+    static int total;
+    public Food() {
+    }
+
+    public static int getCounter() { return counter; }
+
+    public static int getCalories() {
+        return calories;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static int getTotal() {
+        return total;
+    }
+
+    public static void setCounter(int counter) {
+        Food.counter = counter;
+    }
+
+    public static void setCalories(int calories) {
+        Food.calories = calories;
+    }
+
+    public static void setName(String name) {
+        Food.name = name;
+    }
+
+    public static void setTotal(int total) {
+        Food.total = total;
+    }
+
+
     public abstract void removefood(String removename, int removecals) throws InvalidInputException;
    // public abstract void removefood();
 
